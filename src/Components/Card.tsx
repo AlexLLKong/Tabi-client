05fa3777a0c3cd10f1b6921ff95adb6931d58c4c
AlexLLKong <alexkonghockey@gmail.com>
2020-11-07 22:22:37 -0800

    Add design from mockup
    - added support for x number of cards in my trips page
    
    [Ticket: 014]

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,5 +1,9 @@
 import React, { FC } from 'react'
-
+import styles from './Card.module.css'
 type Props = {}
 
-export const Card: FC<Props> = ({ children }) => <div>{children}</div>
+export const Card: FC<Props> = ({ children }) => (
+	<div className={styles.card}>
+		<div className={children ? styles.footer : ''}>{children}</div>
+	</div>
+)
--- src/Pages/MyTripsPage.tsx
+++ src/Pages/MyTripsPage.tsx
@@ -1,11 +1,46 @@
-import React, { FC } from 'react'
+import React, { FC, MouseEvent } from 'react'
 import { Wrapper } from '../Components/Wrapper'
 import { Card } from '../Components/Card'
+import { Button } from '../Components/Button'
+import pageStyles from './Pages.module.css'
+import styles from './MyTripsPage.module.css'
+const tempFn = (e: MouseEvent<HTMLElement>): void => {
+	e.preventDefault()
+	console.log('Placeholder button on click handler clicked')
+}
+
+const generateCards = (numCards: number): JSX.Element[] => {
+	let returnArr: JSX.Element[] = []
+	for (let i: number = 0; i < numCards; i++) {
+		returnArr.push(
+			<Card
+				children={
+					<div>
+						<h3>Example Card {i + 1}</h3>
+						<div className={styles.buttons}>
+							<Button
+								className={styles.btn}
+								onClick={tempFn}
+								children={'Share'}
+							/>
+							<Button
+								className={styles.btn}
+								onClick={tempFn}
+								children={'Delete'}
+							/>
+						</div>
+					</div>
+				}
+			/>
+		)
+	}
+	return returnArr
+}
 export const MyTripsPage: FC = () => (
-	<Wrapper>
-		<h1>My Trips</h1>
+	<div>
 		<Wrapper>
-			<Card children={<h3>Example Card</h3>}></Card>
+			<h1 className={pageStyles.title}>My Trips</h1>
+			<div className={styles.cardContainer}>{generateCards(3)}</div>
 		</Wrapper>
-	</Wrapper>
+	</div>
 )

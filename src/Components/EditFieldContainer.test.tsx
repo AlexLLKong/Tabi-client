afacd164d995f132613b7d5667e268bdbcbb2a36
AlexLLKong <alexkonghockey@gmail.com>
2020-11-09 18:03:22 -0800

    EditFieldContainer passes update on input test
    [Ticket: 017]

--- src/Components/EditFieldContainer.test.tsx
+++ src/Components/EditFieldContainer.test.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent, createEvent } from '@testing-library/react'
 import { EditFieldContainer } from './EditFieldContainer'
-
+const pageContent = (
+	<div>
+		<h1 data-editable="true">Page Title</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+			Voluptates, est?
+		</p>
+	</div>
+)
 test('renders content', () => {
-	const pageContent = (
-		<div>
-			<h1>Page Title</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-				Voluptates, est?
-			</p>
-		</div>
-	)
 	render(<EditFieldContainer content={pageContent} />)
 	screen.getByText(
 		'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, est?'
 	)
+})
+test('default element innerText changes on input', () => {
+	render(<EditFieldContainer content={pageContent} />)
+	const inputElement = screen.getByPlaceholderText('Edit selection')
+	const changeEvent = createEvent.change(inputElement, {
+		target: { value: 'test' },
+	})
+	fireEvent(inputElement, changeEvent)
+	screen.getByText('test')
 })
--- src/Components/EditFieldContainer.tsx
+++ src/Components/EditFieldContainer.tsx
@@ -1,5 +1,35 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 type Props = { content: JSX.Element }
-export const EditFieldContainer: FC<Props> = ({ content }) => (
-	<div>{content}</div>
-)
+const useInput = (type: string): [string, JSX.Element] => {
+	const [value, setValue] = useState('')
+	const input = (
+		<input
+			value={value}
+			onChange={e => setValue(e.target.value)}
+			type={type}
+			placeholder="Edit selection"
+		/>
+	)
+	return [value, input]
+}
+export const EditFieldContainer: FC<Props> = ({ content }) => {
+	let [editElement, setEditElement] = useState<HTMLElement | null>(null)
+	let [newValue, newValueInput] = useInput('text')
+
+	useEffect(
+		() =>
+			setEditElement(editElement =>
+				document.querySelector("[data-editable = 'true']")
+			),
+		[]
+	)
+	useEffect(() => {
+		if (editElement) editElement.innerHTML = newValue
+	}, [newValue, editElement])
+	return (
+		<div>
+			{newValueInput}
+			{content}
+		</div>
+	)
+}
